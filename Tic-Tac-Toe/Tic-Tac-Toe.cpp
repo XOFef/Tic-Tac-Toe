28a3462cb7c2149314ca9b6ebe43b1b635d0a5c2
Keqing <146825224+XOFef@users.noreply.github.com>
2024-09-13 23:21:18 +0300

    Add files via upload

---
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char>& board);
+char winner(const vector<char>& board);
+bool isLegal(const vector<char>& board, int move);
+int humanMove(const vector<char>& board, char human);
+int computerMove(const vector<char>& board, char computer);
+void announceWinner(char winner, char computer, char human);
+
+
+
+
+int main()
+{
+    int move;
+    const int NUM_SQUARES = 9;
+    vector<char> board(NUM_SQUARES, EMPTY);
+    instructions();
+    char human = humanPiece();
+    char computer = opponent(human);
+    char turn = X;
+    displayBoard(board);
+    while (winner(board) == NO_ONE) {
+        if (turn == human) {
+            move = humanMove(board, human);
+            board[move] = human;
+        }
+        else {
+            move = computerMove(board, computer);
+            board[move] = computer;
+        }
+        displayBoard(board);
+        turn = opponent(turn);
+    }
+    announceWinner(winner(board), computer, human);
+    return 0;
+}
+
+
